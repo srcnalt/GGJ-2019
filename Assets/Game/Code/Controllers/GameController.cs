925230bfb97efd00f12c5fc319f5a126ae6adcc5
Sarge <sercan.altundas@gmail.com>
2019-01-26 16:04:36 +0200

    item animations and randomizing selectables

---
+++ Assets/Game/Code/Controllers/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Item[] items;
+    
+    private const int ACTIVE_ITEM_COUNT = 5;
+    private int[] selectedNumbers = new int[ACTIVE_ITEM_COUNT];
+
+    private void Start()
+    {
+        for (int i = 0; i < ACTIVE_ITEM_COUNT; i++)
+        {
+            int randomNum = GetRandomNumber();
+
+            if(!selectedNumbers.Contains(randomNum))
+            {
+                selectedNumbers[i] = randomNum;
+                items[randomNum].isQuestItem = true;
+                
+                Debug.Log(randomNum);
+            }
+            else
+            {
+                i--;
+            }
+        }
+    }
+
+    private int GetRandomNumber()
+    {
+        return Random.Range(0, items.Length);
+    }
+}
--- Assets/Game/Code/Controllers/VehicleController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace DefaultNamespace
-{
-    public class VehicleController : MonoBehaviour
-    {
-        
-    }
-}
