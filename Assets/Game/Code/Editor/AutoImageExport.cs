e163721e29f1e1c303ef95556cda2d242e54afaf
Sarge <sercan.altundas@gmail.com>
2019-01-26 12:04:26 +0200

    big commit

---
+++ Assets/Game/Code/Editor/AutoImageExport.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Game.Code.Editor
+{
+    public class AutoImageExport : AssetPostprocessor
+    {
+        private void OnPreprocessTexture()
+        {
+            var importer = (TextureImporter) assetImporter;
+            
+            Debug.Log(importer.name);
+            
+            if (!importer.assetPath.Contains("Items")) return;
+        
+            importer.filterMode = FilterMode.Point;
+            importer.textureType = TextureImporterType.Sprite;
+            importer.compressionQuality = 0;
+            importer.crunchedCompression = false;
+        }
+    }
+}
